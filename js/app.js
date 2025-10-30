63e7c5cab59fe279f366fcdffac33f4b1621ce24
scifi <leith.huddleston@gmail.com>
2025-10-29 19:05:35 -0600

    generated board, did some css stuff, flood fill next

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@
 
 /*---------------------RESET BUTTON---------------------*/
 
+const resetButton = document.querySelector('#reset')
+resetButton.style.background = 'blue'
 
 /*---------------------DIFFICULTY---------------------*/
 
@@ -19,7 +21,7 @@ class difficultyParams {
 
 const easy = new difficultyParams(9, 9, 10)
 const medium = new difficultyParams(16, 16, 40)
-const hard = new difficultyParams(16, 30, 99)
+const hard = new difficultyParams(30, 16, 99)
 
 let difficulty = easy
 
@@ -35,28 +37,30 @@ for (let i = 0; i < chosenDifficulties.length; i++) {
                 chosenDifficulty.checked = true
             } else {
                 chosenDifficulty.checked = false
-            }
-            
+            }            
         }
+        choseDifficulty()
+        generateBoard()
     })
     
 }
 
 
 /*-----------DIFFICULTY = CHOSEN-----------*/
-for (let i = 0; i < chosenDifficulties.length; i++) {
-    const chosenDifficulty = chosenDifficulties[i];
-    if(chosenDifficulty.checked) {
-        if(chosenDifficulty.id === 'easy') {
-            difficulty = easy
-        } else if(chosenDifficulty.id === 'medium') {
-            difficulty = medium
-        } else if(chosenDifficulty.id === 'hard') {
-            difficulty = hard
-        } 
-    }    
+function choseDifficulty() {
+    for (let i = 0; i < chosenDifficulties.length; i++) {
+        const chosenDifficulty = chosenDifficulties[i];
+        if(chosenDifficulty.checked) {
+            if(chosenDifficulty.id === 'easy') {
+                difficulty = easy
+            } else if(chosenDifficulty.id === 'medium') {
+                difficulty = medium
+            } else if(chosenDifficulty.id === 'hard') {
+                difficulty = hard
+            } 
+        }    
+    }
 }
-
 
 /*---------------------MINES LEFT---------------------*/
 //mines - flags
@@ -73,9 +77,43 @@ for (let i = 0; i < chosenDifficulties.length; i++) {
 const board = document.querySelector('#board')
 
 /*---------------------GENERATE BOARD---------------------*/
+function generateBoard() {
+
+    board.style.gridTemplateColumns = `repeat(${difficulty.x}, 30px)`
+    board.style.gridTemplateRows = `repeat(${difficulty.y}, 30px)`
+
+    while(board.children.length > 0) {
+        board.children[0].remove()
+        console.log(board.children.length)        
+    }
+
+    //try with flood fill?
+
+    for (let i = 0; i < (difficulty.x * difficulty.y); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square', 'closed')
+        board.appendChild(square)
+    }
+}
+
 
 
 /*---------------------POPULATE MINES---------------------*/
+
+const squares = document.querySelectorAll('.square')
+
+//not working - gotta do as flood fill anyways
+
+// function populateMines() {
+//     let minesLeft = difficulty.mines
+//     for (let i = 0; i < squares.length; i++) {
+//         const square = squares[i];
+//         if(Math.random() < (minesLeft / (squares.length - i))) {
+//             square.textContent = 'M'
+//             minesLeft -= 1
+//         }        
+//     }
+// }
 
 
 /*---------------------POPULATE NUMBERS---------------------*/
@@ -128,3 +166,5 @@ const board = document.querySelector('#board')
 
 
 
+generateBoard()
+populateMines()
