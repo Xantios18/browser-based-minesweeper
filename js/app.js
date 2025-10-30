b5292947b256031ec72b60c0596c388a36a05df2
scifi <leith.huddleston@gmail.com>
2025-10-30 16:39:46 -0600

    got mines, fixed bug with difficulty selection, outlining flood fill for numbers

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 /*---------------------RESET BUTTON---------------------*/
 
 const resetButton = document.querySelector('#reset')
-resetButton.style.background = 'blue'
+
 
 /*---------------------DIFFICULTY---------------------*/
 
@@ -31,16 +31,22 @@ const chosenDifficulties = document.querySelectorAll('input[type="checkbox"][nam
 for (let i = 0; i < chosenDifficulties.length; i++) {
     const chosenDifficulty = chosenDifficulties[i];
     chosenDifficulty.addEventListener('click', (event) => {
-        for (let j = 0; j < chosenDifficulties.length; j++) {
-            const chosenDifficulty = chosenDifficulties[j];
-            if(chosenDifficulty.id === event.target.id) {
-                chosenDifficulty.checked = true
-            } else {
-                chosenDifficulty.checked = false
-            }            
-        }
+        if(event.target.checked === true) {
+            for (let j = 0; j < chosenDifficulties.length; j++) {
+                const chosenDifficulty = chosenDifficulties[j];
+                if(chosenDifficulty.id === event.target.id) {
+                    chosenDifficulty.checked = true
+                } else {
+                    chosenDifficulty.checked = false
+                }            
+            }
         choseDifficulty()
         generateBoard()
+        populateMines()
+
+        } else {
+            event.target.checked = true
+        }
     })
     
 }
@@ -83,15 +89,14 @@ function generateBoard() {
     board.style.gridTemplateRows = `repeat(${difficulty.y}, 30px)`
 
     while(board.children.length > 0) {
-        board.children[0].remove()
-        console.log(board.children.length)        
+        board.children[0].remove()               
     }
 
-    //try with flood fill?
 
     for (let i = 0; i < (difficulty.x * difficulty.y); i++) {
         const square = document.createElement('div');
         square.classList.add('square', 'closed')
+        square.textContent = i
         board.appendChild(square)
     }
 }
@@ -100,24 +105,66 @@ function generateBoard() {
 
 /*---------------------POPULATE MINES---------------------*/
 
-const squares = document.querySelectorAll('.square')
 
-//not working - gotta do as flood fill anyways
+function populateMines() {
+    const squares = document.querySelectorAll('.square')
+    let minesLeft = difficulty.mines
 
-// function populateMines() {
-//     let minesLeft = difficulty.mines
-//     for (let i = 0; i < squares.length; i++) {
-//         const square = squares[i];
-//         if(Math.random() < (minesLeft / (squares.length - i))) {
-//             square.textContent = 'M'
-//             minesLeft -= 1
-//         }        
-//     }
-// }
+    for (let i = 0; i < squares.length; i++) {
+        const square = squares[i];
+        if(Math.random() < (minesLeft / (squares.length - i))) {
+            square.textContent = 'M'
+            minesLeft -= 1            
+        }        
+    }
+}
 
 
 /*---------------------POPULATE NUMBERS---------------------*/
 
+//NAVIGATING GRID BASED ON SQUARES[i]
+    //right = + 1
+        //can't go any further right if (i + 1) % x === 0
+    //left = - 1
+        //can't go any further left if i % x === 0
+    //down = + x
+        //can't go any further down if i >= (x * (y - 1))
+    //up = - x
+        //can't go any further up if i < x
+
+function populateNumbers(i) {
+    const squares = document.querySelectorAll('.square')
+
+    /*----------BOUNDARY CONDITIONS----------*/
+    if(i % document.x === 0) {
+        //left edge
+    } else if((i + 1) % document.x === 0) {
+        //right edge
+    } else if(i < document.x) {
+        //top edge
+    } else if(i >= document.x * (document.y - 1)) {
+        //bottom edge
+    } else if(i === 0) {
+        //top left
+    } else if(i === document.x * (document.y - 1)) {
+        //botom left
+    } else if(i === document.x - 1) {
+        //top right
+    } else if(i === (document.x * document.y) - 1) {
+        //botom right
+    } else {
+        //central square
+    }
+
+    /*----------ACTUAL FUNCTION----------*/
+    
+
+
+    /*----------RECURSION----------*/
+
+
+
+}
 
 /*---------------------CHECK FOR VICTORY---------------------*/
 
@@ -167,4 +214,5 @@ const squares = document.querySelectorAll('.square')
 
 
 generateBoard()
-populateMines()
+// populateMines()
+// populateNumbers()
